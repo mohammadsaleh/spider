465423e66a1bc7be15686f7edc42ed4e3c659021
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2016-06-05 20:34:16 +0430

    usershelper

--- Users/src/View/Helper/UsersHelper.php
+++ Users/src/View/Helper/UsersHelper.php
@@ -5,7 +5,12 @@ use Cake\View\Helper;
 
 class UsersHelper extends Helper
 {
-    
+
+    public function isLogin()
+    {
+        return $this->request->session()->check('Auth.User') ? $this->request->session()->read('Auth.User') : false;
+    }
+
     public function getFullName()
     {
         $currentUser = $this->request->session()->read('Auth.User');
