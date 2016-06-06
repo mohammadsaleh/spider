8d78cb3e4b3910842878449c42619f20181bf26b
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2016-06-06 14:02:31 +0430

    check if user has given role or not

--- AclManager/src/Controller/Component/CustomAuthComponent.php
+++ AclManager/src/Controller/Component/CustomAuthComponent.php
@@ -2,8 +2,22 @@
 namespace AclManager\Controller\Component;
 
 use Cake\Controller\Component\AuthComponent;
+use Cake\Utility\Hash;
 
 class CustomAuthComponent extends AuthComponent
 {
 
+    /**
+     * Check if user has given role or not
+     * @param String/Integer $role
+     * @return bool
+     */
+    public function hasRole($role)
+    {
+        $userRoles = Hash::extract($this->user('roles'), '{n}.name');
+        if(is_integer($role)){
+            $userRoles = Hash::extract($this->user('roles'), '{n}.id');
+        }
+        return in_array($role, $userRoles);
+    }
 }
