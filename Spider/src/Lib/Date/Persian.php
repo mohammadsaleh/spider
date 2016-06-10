87ac5e772ed03648993461c95b84549a63e8e15c
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2016-06-10 14:56:11 +0430

    diff day

--- Spider/src/Lib/Date/Persian.php
+++ Spider/src/Lib/Date/Persian.php
@@ -9,4 +9,18 @@ class Persian
     {
         return call_user_func_array('jdate', func_get_args());
     }
+
+    /**
+     * @param $date1
+     * @param $date2
+     * @param string $format : DateInterval::format http://php.net/manual/en/dateinterval.format.php
+     * @return string
+     */
+    public static function diff($date1, $date2, $format = 'd')
+    {
+        $dStart = new \DateTime($date1);
+        $dEnd  = new \DateTime($date2);
+        $dDiff = $dStart->diff($dEnd);
+        return $dDiff->format('%' . $format); // use for point out relation: smaller/greater
+    }
 }
