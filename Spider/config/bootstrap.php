176c51789f5860fc6e23514d3763ff4164d89c3b
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2016-12-04 18:59:55 +0330

    fixed dashboard route

--- Spider/config/bootstrap.php
+++ Spider/config/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 use Cake\Core\Plugin;
 use Cake\I18n\I18n;
-use Spider\Lib\SpiderNav;
 use Cake\Network\Request;
 use Spider\Lib\Hook;
 
@@ -13,14 +12,6 @@ Request::addDetector(
     'chrome',
     ['env' => 'HTTP_USER_AGENT', 'pattern' => '/Chrome/i']
 );
-
-
-SpiderNav::add('sidebar', 'Dashboard' ,[
-    'title' => __d('spider', 'Dashboard'),
-    'url' => SpiderNav::getDashboardUrl(),
-    'icon' => 'fa fa-home',
-    'weight' => -10
-]);
 
 I18n::locale('fa_IR');
 Plugin::load('BootstrapUI');
--- Spider/config/routes.php
+++ Spider/config/routes.php
@@ -2,6 +2,14 @@
 use Cake\Routing\Router;
 use Spider\Lib\SpiderNav;
 
+//todo:: best is that dashboard url would be set on admin settings.
+SpiderNav::add('sidebar', 'Dashboard' ,[
+    'title' => __d('spider', 'Dashboard'),
+    'url' => SpiderNav::getDashboardUrl(),
+    'icon' => 'fa fa-home',
+    'weight' => -10
+]);
+
 Router::plugin('Spider', function ($routes) {
     $routes->fallbacks('InflectedRoute');
 });
