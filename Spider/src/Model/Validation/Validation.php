5e48754038ea9f58558e844bf30cb28b6188d0ec
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2017-01-17 10:59:17 +0330

    new changes

--- Spider/src/Model/Validation/Validation.php
+++ Spider/src/Model/Validation/Validation.php
@@ -16,10 +16,53 @@ class Validation extends CakeValidation
 
 	public static function persianAlpha($check)
 	{
-		if (empty($check) && $check !== '0') {
+		$reg = '/^[\s\x{600}-\x{6FF}]+$/u';
+		return self::_check($check, $reg);
+	}
+
+	public static function nationalCode($check)
+	{
+		if((strlen($check) != 10)){
 			return false;
+		}else{
+			$codeArray = str_split($check);
+			$AllEq = null;
+			foreach($codeArray as $item => $value){
+				if($codeArray[0] != $value){
+					$AllEq = false;
+					break;
+				}else{
+					$AllEq = true;
+				}
+			}
+			if($AllEq == true){
+				return false;
+			};
+			$weight = 10;
+			$sum = 0;
+			for($i = 0; $i <= 8; $i++){
+				$sum +=((int)($codeArray[$i])) * $weight;
+				--$weight;
+			}
+			$divid = $sum % 11;
+			if ($divid <= 2) {
+				if($codeArray[9]  == $divid) {
+					return true;
+				}
+				return false;
+			}else{
+				$divid1 = 11 - $divid;
+				if ($codeArray[9]  == $divid1){
+					return true;
+				}
+				return false;
+			}
 		}
-		$reg = '[ آ-ی]+';
-		return self::_check($check, '/^' . $reg . '$/');
+	}
+
+	public static function mobile($check)
+	{
+		$reg = '/^09(1[0-9]|3[1-9]|2[1-9])?[0-9]{7}$/' ;
+		return self::_check($check, $reg);
 	}
 }
