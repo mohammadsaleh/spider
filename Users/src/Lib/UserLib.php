ab52941f729f3936b8716f2cc6b6c767442793e8
MohammadSaleh <mohammadsaleh.sayari@gmail.com>
2017-06-11 12:50:08 +0430

    get superadmin user

--- Users/src/Lib/UserLib.php
+++ Users/src/Lib/UserLib.php
@@ -1,6 +1,8 @@
 <?php
 namespace Users\Lib;
 
+use Cake\ORM\TableRegistry;
+
 class UserLib
 {
     private static $__Auth = null;
@@ -12,6 +14,16 @@ class UserLib
     public static function getAuth()
     {
         return self::$__Auth;
+    }
+
+    public static function getSuperAdmin()
+    {
+        $superAdminUser = TableRegistry::get('Users.Users')->find()
+            ->matching('Roles', function ($q){
+                return $q->where(['name' => 'superadmin']);
+            })
+            ->first();
+        return $superAdminUser;
     }
 }
 
